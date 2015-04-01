5d4785e433e527461658301721049f555691c9c5
Dzenan Ridjanovic <dzenanr@gmail.com>
2015-04-01 12:56:05 -0400

    update to boarding 0.2.4 & add doc/dr.txt

--- lib/view/board.dart
+++ lib/view/board.dart
@@ -6,26 +6,26 @@ class Board extends Surface {
   var blueCars = new MovablePieces(5);
   var myCar = new MovablePiece(1);
   var big = true;
-  
+
   Board(CanvasElement canvas) : super(canvas) {
-    initVehicule(myCar, canvas.width / 2 - myCar.width, canvas.height / 2 - myCar.height, 
+    initVehicule(myCar, canvas.width / 2 - myCar.width, canvas.height / 2 - myCar.height,
         100, 40, "red");
-    
+
     document.onMouseMove.listen((MouseEvent e) {
       myCar.x = e.offset.x - myCar.width  / 2;
-      myCar.y = e.offset.y - myCar.height / 2; 
+      myCar.y = e.offset.y - myCar.height / 2;
       myCar.move();
     });
-    
+
     document.onMouseDown.listen((MouseEvent e) {
       if (big == false) {
         myCar.width = 80;
         myCar.height = 60;
-        myCar.color = "red";
+        myCar.color.main = "red";
         big = true;
       }
     });
-   
+
     var y = 0;
     blueCars.forEach((car) {
       initVehicule(car, 0, 0, 80, 40, "blue");
@@ -36,30 +36,30 @@ class Board extends Surface {
       }
     });
   }
-  
+
   initVehicule(MovablePiece mp, num y, num x, num width, num height, String color) {
     mp.shape = PieceShape.VEHICLE;
     mp.y = y;
     mp.x = x;
     mp.width = width;;
     mp.height = height;
-    mp.space = new Size(canvas.width, canvas.height);
-    mp.color = color;
+    mp.space = new Area(canvas.width, canvas.height);
+    mp.color.main = color;
     mp.speed.dx = randomNum(6);
     mp.speed.dy = randomNum(6);
   }
-      
+
   draw() {
-    clear();   
+    clear();
     blueCars.forEach((car) {
       car.move();
       blueCars.avoidCollisions(car);
       if (big == true && myCar.hit(car) == true) {
         myCar.width = 20;
         myCar.height = 20;
-        myCar.color = "black";
+        myCar.color.main = "black";
         big = false;
-      }          
+      }
       drawPiece(car);
     });
     drawPiece(myCar);
